0c07d2993a19f07007b29d9627b1498e0aa83b5d
arcbjorn <arky.happ@gmail.com>
2021-06-02 19:52:01 +0300

    feat (i3wm): update & add GUI packages

--- i3wm.sh
+++ i3wm.sh
@@ -1,61 +1,71 @@
-# #!/bin/bash
+#!/bin/bash
 
-# sudo timedatectl set-ntp true
-# sudo hwclock --systohc
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
 
-# sudo reflector -a 12 --sort rate --save /etc/pacman.d/mirrorlist
-# sudo pacman -Syy
+sudo reflector -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Syy
 
-# sudo firewall-cmd --add-port=1025-65535/tcp --permanent
-# sudo firewall-cmd --add-port=1025-65535/udp --permanent
-# sudo firewall-cmd --reload
-# # sudo virsh net-autostart default
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+# sudo virsh net-autostart default
 
-# echo "MAIN PACKAGES FOR i3wm"
+echo "MAIN PACKAGES FOR i3wm"
 
-# # install DE packages from AUR:
+# install DE packages from offical repos:
 
-# # Window Manager Sway (sway-borders fork)
-# # Login Manager
-# # Web Browser
-# # System backup & restore tool
+# xorg display server
+sudo pacman -S --noconfirm xorg
+# window manager i3wm (i3-gaps fork)
+sudo pacman -S --noconfirm i3-gaps
+# login manager
+sudo pacman -S --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+# window compositor
+sudo pacman -S --noconfirm picom
+# app launcher & dynamic menu
+sudo pacman -S --noconfirm rofi
+# system notifications manager
+sudo pacman -S --noconfirm dunst
+# theme customization, colour generator, wallpaper manager
+sudo pacman -S --noconfirm lxappearance python-pywal nitrogen
+# terminal emulator, file manager, password manager
+sudo pacman -S --noconfirm kitty pcmanfm-qt pass
+# CLI to display system specs
+sudo pacman -S --noconfirm neofetch
 
-# paru -S --noconfirm sway-borders-git ly-git firefox-nightly timeshift timeshift-autosnap
+# remove default lockscreen for i3wm
+sudo pacman -R --noconfirm i3lock
 
-# # install DE packages from offical repos:
+# install DE packages from AUR:
 
-# # App Launcher
-# # Theme customization package
-# # Theme color generator
-# # Status Bar
-# # File manager
-# # Password manager
-# # Terminal emulator
-# # CLI to display system specs
-# # System notifications manager
+# status bar & lockscreen packages
+paru -S --noconfirm polybar betterlockscreen
+# web browser, system backup & restore tool
+paru -S --noconfirm firefox-nightly timeshift timeshift-autosnap
 
-# sudo pacman -S wofi lxappearance python-pywal waybar pcmanfm-qt pass foot neofetch dunst
+# enable login screen on boot
+sudo systemctl enable lightdm
 
-# # enable login screen on boot
-# sudo systemctl enable ly.service
+# Default Configuration
 
-# mkdir -p .config/{sway,dunst,waybar,wofi}
+mkdir -p .config/{dunst,i3,rofi,polybar}
 
-# install -Dm755 /etc/sway/config ~/.config/sway/
-# install -Dm755 /etc/dunst/dunstrc ~/.config/dunst/
-# install -Dm755 /usr/share/foot/foot.ini ~/.config/foot/
+install -Dm755 /etc/xdg/picom.conf ~/.config/picom.conf
+install -Dm755 /usr/share/doc/betterlockscreen/examples/betterlockscreenrc ~/.config/betterlockscreenrc
 
-# touch ~/.config/waybar/config
-# touch ~/.config/wofi/config
+install -Dm755 /etc/dunst/dunstrc ~/.config/dunst/dunstrc
+# i3 config: run i3-config-wizard or edit manually
+install -Dm755 /etc/i3/config ~/.config/i3/config
+# rofi config: generate config or check the config commands: rofi -dump-config
+rofi -dump-config > ~/.config/rofi/config.rasi
+# kitty config: ~/.config/kitty/kitty.conf
+install -Dm755 /usr/share/doc/polybar/config ~/.config/polybar/config
 
-# # cp /etc/ly/config.ini ~/.config/ly/
-# # config for ly: /etc/ly/config.ini
+# manual config for lightdm: /etc/lightdm/lightdm.conf.d/50-myconfig.conf
+# otherwise lightdm-gtk-greeter-settings preferred
+# example:
+# [Seat:*]
+# user-session=mysession
 
-# # set environment variables to use Wayland
-# echo "QT_QPA_PLATFORM=wayland" >> /etc/environment
-# echo "MOZ_ENABLE_WAYLAND=1" >> /etc/environment
-# echo "MOZ_WEBRENDER=1" >> /etc/environment
-# echo "XDG_SESSION_TYPE=wayland" >> /etc/environment
-# echo "XDG_CURRENT_DESKTOP=sway" >> /etc/environment
-
-# printf "\e[1;32mCHANGE NECESSARY FILES BEFORE REBOOT, THEN RUN REBOOT COMMAND\e[0m"
+printf "\e[1;32mCHANGE NECESSARY FILES BEFORE REBOOT, THEN RUN REBOOT COMMAND\e[0m"
